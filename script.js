2a7fdafe7fd36ae14cb7a5e66c9fa54cf1b641a0
Lewis Ng’ate <lewisngate@Lewiss-MacBook-Air.local>
2023-03-03 12:12:29 +0300

    build to the akan name

---
+++ script.js
@@ -0,0 +1,18 @@
+const maleAkanNames{
+    Sunday: Kwasi, 
+    Monday: Kwadwo,
+    Tuesday: Kwabena,
+    Wednesday: Kwaku,
+    Thursday:  Yaw,
+    Friday: Kofi,
+    Saturday: Kwame
+}
+const femaleAkanNames{
+    Sunday: Akosua,
+    Monday: Adwoa,
+    Tuesday: Abenaa,
+    Wednesday: Akua,
+    Thursday:  Yaa,
+    Friday: Afua,
+    Saturday: Ama
+}
