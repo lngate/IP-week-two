bbfec3b546c7305f28ff89ebe38df86d41912598
Lewis Ng’ate <lewisngate@Lewiss-MacBook-Air.local>
2023-03-09 12:10:06 +0300

    splitting date so that can give akan name

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function viewdate() {   
-    var x = document.getElementById("birthday").value;   
-    document.getElementById("demo").innerHTML = x;   
+    var showBirthdate = document.getElementById("birthday").value;   
+    document.getElementById("viewDate").innerHTML = showBirthdate;   
     }
 
-// var date = birthDate;
-// var arr = date.split(/ |,/);
-// console.log('date: ', arr[0]);
-// console.log('month: ', arr[1]);
-// console.log('year: ', arr[2]);
+var date = showBirthdate;
+const arr = date.split("-");
+console.log('year: ', arr[0]);
+console.log('month: ', arr[1]);
+console.log('Date: ', arr[2]);
 
-// document.getElementById("birthday").addEventListener("change", function() {
-//     var input = this.value;
-//     var dateEntered = new Date(input);
-//     console.log(input); //e.g. 2015-11-13
-//     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
-// });
